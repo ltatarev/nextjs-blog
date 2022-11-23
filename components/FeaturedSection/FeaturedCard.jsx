f2b0d4a7900e99fe17e95a59d40fd3cf8c24a8c9
Lucija Tatarević <lucija.tatarevic@gmail.com>
2022-11-23 01:38:00 +0100

    Tinker with styles

--- components/FeaturedSection/FeaturedCard.jsx
+++ components/FeaturedSection/FeaturedCard.jsx
@@ -8,32 +8,40 @@ export const CARD_TYPES = ['BOOK', 'CURRENT_READ', 'TV', 'GAME'];
 export function FeaturedCard({
   title, label, imgSrc, AccentImage, main,
 }) {
+  const containerClasses = cn(
+    `mx-20
+    flex
+    w-1/5
+    flex-col
+    items-center`,
+    { 'w-1/3': main },
+  );
+
   const innerContentClasses = cn(
     `flex
     flex-col
+    w-full
     items-center
-    align-middle
     rounded-2xl
     bg-white
     p-8
-    text-center
-    shadow-sm
+    shadow-lg
     hover:-translate-y-0.5
     hover:shadow-xl
     hover:duration-100`,
-    { 'h-[22rem] px-12 -my-6': main },
+    { 'h-[25rem] w-4/6 -my-6': main },
+  );
+
+  const imageContainerClasses = cn(
+    'relative h-60 w-full flex justify-center px-2',
+    { 'h-5/6 mt-5': main },
   );
 
   const imageWidth = main ? 320 : 150;
   const imageHeight = main ? 300 : 100;
 
   return (
-    <div className="mx-20
-      flex
-      h-3/4
-      w-1/6
-      flex-col"
-    >
+    <div className={containerClasses}>
       <div
         className={innerContentClasses}
       >
@@ -53,12 +61,14 @@ export function FeaturedCard({
           {label}
         </p>
         )}
-        <Image
-          className="rounded-xl object-cover py-1"
-          height={imageHeight}
-          src={imgSrc}
-          width={imageWidth}
-        />
+        <div className={imageContainerClasses}>
+          <Image
+            className="rounded-2xl object-cover my-2"
+            height={imageHeight}
+            src={imgSrc}
+            width={imageWidth}
+          />
+        </div>
       </div>
       {AccentImage}
     </div>
@@ -69,11 +79,12 @@ export function FeaturedCard({
 FeaturedCard.propTypes = {
   AccentImage: PropTypes.node.isRequired,
   imgSrc: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  label: PropTypes.string,
   main: PropTypes.bool,
 };
 
 FeaturedCard.defaultProps = {
+  label: null,
   main: false,
 };
